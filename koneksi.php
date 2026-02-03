f1bb0641b299c615385591b24b0aef035b3b143c
laylamulyani700-creator <laylamulyani700@gmail.com>
2026-02-03 14:39:54 +0800

    Initial commit

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$koneksi = mysqli_connect("localhost","root","","perpus");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+?>
